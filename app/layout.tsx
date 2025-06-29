0274534574c653622cdec0e9bf1432fafd996a4a
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-06-29 17:05:51 +0300

    init & limited height of columns

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Nivalla Tasks',
+  description: 'The modern task management app',
 }
 
 export default function RootLayout({
