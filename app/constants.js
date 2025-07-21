77f5fed242570e12498aff24efb26d73133e9fb8
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-07-21 16:56:02 +0300

    switched to vercel api url

--- app/constants.js
+++ app/constants.js
@@ -1,9 +1,4 @@
 // export const baseUrl = "https://nirvalla-tasks.up.railway.app/api/v1";
-// export const baseUrl = "https://nv-tasks-api.vercel.app/api/v1";
-export const baseUrl = "http://localhost:3001/api/v1";
-
-// export const token =
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODYyNjM4M2RhNWRiMWRhNTg2NzQ3YzIiLCJ1c2VyTmFtZSI6InRlc3QiLCJpYXQiOjE3NTEyNzkwNzYsImV4cCI6MTc4MjgxNTA3Nn0.STDifspT-Dcsn4qVjIodz2yDdF8Tm2pMUt6DZmfAZ-4";
-
-// export const token = ""
+export const baseUrl = "https://nv-tasks-api.vercel.app/api/v1";
+// export const baseUrl = "http://localhost:3001/api/v1";
 
